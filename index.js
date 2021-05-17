b6466819b2d0849198f359eaf6484790103fe398
abo3zah <31366659+abo3zah@users.noreply.github.com>
2021-05-17 13:55:12 +0300

    Nodejs

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const app = express();
 
 app.get('/' , async (req , res)=>{
     
-    await readFile('./main.html','utf8',(err, html)=>{
+    res.send(await readFile('./main.html','utf8',(err, html)=>{
         if (err){
             res.status(500).send('Sorry, Out of Order');
         }
         res.send(html);  
-    })
+    }))
 })
 
 app.listen(process.env.PORT || 3000, () => console.log('App available'))
