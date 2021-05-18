da0e2d48bf3dd47c64de42803711d43369e447f5
abo3zah <31366659+abo3zah@users.noreply.github.com>
2021-05-18 13:22:32 +0300

    file

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const { readFile } = require('fs');
 
 const app = express();
 
-app.get('/' , async (req , res)=>{
+app.get('/' , (req , res)=>{
     
-    res.send(await readFile('./main.html','utf8',(err, html)=>{
+    readFile('./main.html','utf8',(err, html)=>{
         if (err){
             res.status(500).send('Sorry, Out of Order');
         }
         res.send(html);  
-    }))
+    })
 })
 
 app.listen(process.env.PORT || 3000, () => console.log('App available'))
